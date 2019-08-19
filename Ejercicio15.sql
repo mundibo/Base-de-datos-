24d3037febe80293df2cb28c1fc86177abd9e984
mundibo <43122143+mundibo@users.noreply.github.com>
2019-08-19 19:57:32 -0400

    Create Ejercicio15.sql

---
+++ Ejercicio15.sql
@@ -0,0 +1,9 @@
+drop table peliculas if exists;
+create table peliculas(
+codigo integer unsigned auto_increment,
+titulo varchar(40) not null,
+actor varchar(20),
+duracion integer unsigned,
+ primary key (codigo)
+ );
+describe peliculas;
